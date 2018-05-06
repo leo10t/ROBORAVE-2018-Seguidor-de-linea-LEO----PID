1fc8f458faa6dc7e44f5ba4864cc857a142b3240
leo10t <37906430+leo10t@users.noreply.github.com>
2018-05-05 22:11:49 -0300

    Create Infrarrojo.h

---
+++ Infrarrojo.h
@@ -0,0 +1,38 @@
+class Infrarrojo {  
+public:
+  Infrarrojo (int p,int b); 
+  bool lee_blanco();
+  bool lee_negro();
+  int diff();
+
+private:
+  int pin;  
+  int blanco;
+};
+
+Infrarrojo::Infrarrojo(int p, int b)
+{
+  pin = p;
+  blanco = b;
+  pinMode(pin,INPUT);
+}
+
+bool Infrarrojo::lee_blanco()
+{
+  if(analogRead(pin) >= blanco)
+    return true;
+ else return false;
+}
+
+bool Infrarrojo::lee_negro()
+{
+  if(analogRead(pin) < blanco)
+    return true;
+   else return false;
+}
+
+int Infrarrojo::diff()
+{
+  return blanco-analogRead(pin);
+}
+
